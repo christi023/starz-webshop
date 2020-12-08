3a4a731eabb4460e7cb11f42516b4b0d9398c2b2
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-12-08 12:40:18 +0100

    Added products and json file

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 import HomeImg from '../../assets/shoe3.jpg';
 // component
-import Product from '../Product';
-
+import Product from '../Product/Product';
+// json products
+import products from '../../../src/products';
 //styles
 import './Home.css';
 
@@ -14,7 +16,14 @@ const Home = () => {
           <h1>Welcome to Starz Webshop</h1>
         </div>
       </div>
-      <Product />
+      <h1>Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
     </>
   );
 };
