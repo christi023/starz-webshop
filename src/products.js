7f00783eff1f2ac49b1f3fb456ef1ab004d9be82
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-12-11 13:33:49 +0100

    products json

---
+++ src/products.js
@@ -0,0 +1,112 @@
+const products = [
+  {
+    id: 1,
+    img: 'images/shoes1.png',
+    title: 'Nike Air Max 2020',
+    price: '100',
+    details:
+      'Nike air max 2020 is the newest trend and design from the company with best materials.It provides the best feeling for your foots and and your health',
+    quantity: 1,
+    isInCart: false,
+  },
+
+  {
+    id: 2,
+    img: 'images/shoes2.png',
+    title: 'Blue Casual shoes',
+    price: '12',
+    details:
+      'Blue casual shoes with a very nice outfit and design. This model provides you a cool fit of your foots and are suitable for every occasion',
+    quantity: 1,
+    isInCart: false,
+  },
+
+  {
+    id: 3,
+    img: 'images/shoes3.png',
+    title: 'Red Women shoes',
+    price: '65',
+    details:
+      'red women shoes for every lady. This shoes are suitable for more official events. With this shoes you will gain all the attention around you (not negative)',
+    quantity: 1,
+    isInCart: false,
+  },
+
+  {
+    id: 4,
+    img: 'images/shoes4.png',
+    title: 'Snow shoes',
+    price: '13',
+    details:
+      'White snow shoes for every difficult ocassions which has to do with snow sports. Very good fit and feeling for your foots.',
+    quantity: 1,
+    isInCart: false,
+  },
+
+  {
+    id: 5,
+    img: 'images/shoes5.png',
+    title: 'Brown Snickers shoes',
+    price: '45',
+    details:
+      'This shoes has been created from the best materials and provides you a good feeling for your foots. Are good for mountains and every hard occasions.',
+    quantity: 1,
+    isInCart: false,
+  },
+
+  {
+    id: 6,
+    img: 'images/shoes6.png',
+    title: 'Black Alien shoes',
+    price: '67',
+    details:
+      'Black aliens shoes are one of the best trends nowadays.The design and fit are very nice and the only sure thing is that you will love wearing it.',
+    quantity: 1,
+    isInCart: false,
+  },
+
+  {
+    id: 7,
+    img: 'images/shoes7.png',
+    title: 'Nike Pigasous',
+    price: '23',
+    details:
+      'Nike pigasous is another interesting shoes model from the nike company. It is very nice for your foots and surely you will not regret your choice.',
+    quantity: 1,
+    isInCart: false,
+  },
+
+  {
+    id: 8,
+    img: 'images/shoes8.png',
+    title: 'Bob Marley shoes',
+    price: '15',
+    details:
+      'Bob marley shoes is a special design shoes in the memory of the the famous singer .This shoes provides you a good feeling of your foots.',
+    quantity: 1,
+    isInCart: false,
+  },
+
+  {
+    id: 9,
+    img: 'images/shoes9.png',
+    title: 'Louis Vuitton shoes',
+    price: '55',
+    details:
+      'Louis Vuitton Run Away Sneaker Charcoal in color Louis Vuitton Sneakers Men Black. Great luxury sneakers made with high quality materials.Fun, fresh, or fabulous shoes complete an outfit. Find your perfect pair with updates on the hottest trends.',
+    quantity: 1,
+    isInCart: false,
+  },
+
+  {
+    id: 10,
+    img: 'images/shoes10.png',
+    title: 'Cherry Blossom shoes',
+    price: '15',
+    details:
+      'Nice Cheap Dunk High Pro Cherry Blossom - Nike Mens Sb Dunk High Pro Cherry Blossom Sneakers.The collection comes in a limited edition and features an exclusive update of the Cherry',
+    quantity: 1,
+    isInCart: false,
+  },
+];
+export default products;
