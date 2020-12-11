dad3ddd256829718b2bfdedd274464dcabdeae10
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-12-11 13:32:12 +0100

    Holds main components and Routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Components
-import Header from './Components/Header';
+import Header from './Components/Navbar/Header';
 import Home from './Components/Home/Home';
-import About from './Components/About';
-import Products from './Components/Products/Products';
+import About from './Components/About/About';
+//import Products from './Components/Products/Products';
+import ProductDetails from './Components/ProductDetails/ProductDetails';
 import Cart from './Components/Cart/Cart';
-import Footer from './Components/Footer';
+//import Footer from './Components/Footer';
 
 import './App.css';
 
@@ -18,13 +19,11 @@ function App() {
       <main className="py-3">
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/products" component={Products} />
-          <Route path="/product/:id" component={Products} />
+          <Route exact path="/product/:id" component={ProductDetails} />
           <Route exact path="/cart" component={Cart} />
           <Route exact path="/about" component={About} />
         </Switch>
       </main>
-      <Footer />
     </Router>
   );
 }
